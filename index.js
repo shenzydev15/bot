450ffdaa7aa46d62e012a076490a8e55525b1007
shenzydev15 <120827854+shenzydev15@users.noreply.github.com>
2023-01-03 16:22:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Intents, MessageEmbed, MessageAttachment, MessageActionRow, MessageButton, PermissionOverwrites, MessageSelectMenu, Interaction, Guild, Collector, Permissions, Collection,  } = require('discord.js')
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] })
 bot.on("ready", ()=> {
-    console.log("Bin online FUN BOT!!")
+    console.log(`${bot.user.tag}`)
 })
 bot.login("YOUR_BOT_TOKEN")
